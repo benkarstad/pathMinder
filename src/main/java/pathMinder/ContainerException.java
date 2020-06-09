413b442e80de3f0f50333495a725c554ba68707d
Benjamin Karstad <benkarstad@gmail.com>
2020-06-09 15:00:08 -0600

    added ContainerException super class for all Container-logic exceptions;

---
+++ src/main/java/pathMinder/ContainerException.java
@@ -0,0 +1,11 @@
+package pathMinder;
+
+/**
+ * Signals that the requested operation could not be performed by a container.
+ * This is the general class of exceptions produced internally by Containers.
+ */
+public class ContainerException extends RuntimeException {
+	public ContainerException(String message) { super(message); }
+	public ContainerException(Throwable cause) { super(cause); }
+	public ContainerException(String message, Throwable err) { super(message, err); }
+}
--- src/main/java/pathMinder/TooManyItemsException.java
+++ src/main/java/pathMinder/TooManyItemsException.java
@@ -1,15 +1,17 @@
 package pathMinder;
 
 /**
- *
+ * Thrown to indicate that a Container cannot accept the supplied items due to
+ * constraints on the quantity that it can contain.
  */
-public class TooManyItemsException extends RuntimeException {
+public class TooManyItemsException extends ContainerException {
 	/**
 	 * TODO: Add a unique serial id
 	 */
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public TooManyItemsException(String message) { super(message); }
+	public TooManyItemsException(Throwable cause) { super(cause); }
 	public TooManyItemsException(String message, Throwable err) { super(message, err); }
 }
